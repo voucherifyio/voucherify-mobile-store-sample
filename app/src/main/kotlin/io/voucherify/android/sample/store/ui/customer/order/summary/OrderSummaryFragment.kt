934507446b9a1a364b77ca192ae8cb8955ce46c4
polok <mpolak87@gmail.com>
2019-04-08 06:06:44 +0200

    Add simple order placeholder view (#27)

---
+++ app/src/main/kotlin/io/voucherify/android/sample/store/ui/customer/order/summary/OrderSummaryFragment.kt
@@ -0,0 +1,52 @@
+package io.voucherify.android.sample.store.ui.customer.order.summary
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import io.voucherify.android.sample.store.R
+import io.voucherify.android.sample.store.ui.base.BaseFragment
+import io.voucherify.android.sample.store.ui.customer.order.OrderViewDelegate
+import io.voucherify.android.sample.store.ui.flow.Navigator
+import kotlinx.android.synthetic.main.fragment_order_summary.*
+import javax.inject.Inject
+
+class OrderSummaryFragment : BaseFragment() {
+
+    companion object {
+        const val TAG = "OrderSummaryFragment"
+
+        fun newInstance() = OrderSummaryFragment()
+    }
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    private var parentViewDelegate: OrderViewDelegate? = null
+
+    override fun fragmentLayoutId(): Int = R.layout.fragment_order_summary
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+
+        parentViewDelegate = activity as OrderViewDelegate
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViews()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        parentViewDelegate?.onTitleChange(getString(R.string.title_order_summary))
+    }
+
+    private fun setViews() {
+        order_summary_buy_button.setOnClickListener {
+            parentViewDelegate?.onBuyClick()
+        }
+    }
+
+}
