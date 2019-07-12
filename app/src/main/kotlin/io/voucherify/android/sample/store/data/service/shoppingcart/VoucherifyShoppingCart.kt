fd2a3caf5bea7684680272b76cb432d1aa5b2d5b
marcin.polak <mpolak87@gmail.com>
2019-07-12 12:36:07 +0200

    Apply voucherify's UI style for shopping cart list

--- app/src/main/kotlin/io/voucherify/android/sample/store/data/service/shoppingcart/VoucherifyShoppingCart.kt
+++ app/src/main/kotlin/io/voucherify/android/sample/store/data/service/shoppingcart/VoucherifyShoppingCart.kt
@@ -4,18 +4,54 @@ import io.voucherify.android.sample.store.data.remote.api.response.ProductRespon
 
 class VoucherifyShoppingCart : ShoppingCartService {
 
-    private val items = mutableListOf<Pair<ProductResponse, Int>>()
+    private val items = mutableListOf<Triple<ProductResponse, Int, Int>>()
 
-    override fun items(): List<Pair<ProductResponse, Int>> {
+    override fun items(): List<Triple<ProductResponse, Int, Int>> {
         return items.toList()
     }
 
     override fun add(item: Pair<ProductResponse, Int>) {
-        items.add(item)
+
+        var itemToUpdate = items.find {
+            it.first == item.first && it.second == item.second
+        }
+
+        val index = items.indexOf(itemToUpdate)
+
+        if (itemToUpdate != null && index > -1) {
+
+            itemToUpdate = itemToUpdate.copy(
+                itemToUpdate.first,
+                itemToUpdate.second,
+                itemToUpdate.third + 1)
+
+            items[index] = itemToUpdate
+        } else {
+            items.add(Triple(item.first, item.second, 1))
+        }
     }
 
     override fun remove(item: Pair<ProductResponse, Int>) {
-        items.remove(item)
+
+        var itemToUpdate = items.find {
+            it.first == item.first && it.second == item.second
+        }
+
+        val index = items.indexOf(itemToUpdate)
+
+        if (itemToUpdate != null && index > -1) {
+
+            if(itemToUpdate.third == 1) {
+                items.remove(itemToUpdate)
+            } else {
+                itemToUpdate = itemToUpdate.copy(
+                    itemToUpdate.first,
+                    itemToUpdate.second,
+                    itemToUpdate.third - 1)
+
+                items[index] = itemToUpdate
+            }
+        }
     }
 
     override fun clear() {
